f1d65bdeb895a93f83f95321c07ef21da0aef7b0
thedesignerkumar <thedesignerkumar@gmail.com>
2016-11-18 02:35:27 +0530

    all design done expect back face visibility for the paragraph on front face

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -2,77 +2,17 @@
 
 jQuery(document).ready(function($) {
     
-    var device_width = $(window).width();
+    var image_height = $('.gallery img').height();
+    var background_para_height = $('.back').height();
+    var background_padding = image_height - background_para_height;
+    // console.log(background_padding);
+    // console.log(background_para_height);
 
-    // mobile nav menu script
-
-    $("#menu").on('click', function(event) {
-
-        // increase the width of the nav.mobile
-        $("nav.mobile").animate({
-            'width': '250px',
-            'direction': 'left'
-        }, 500);
-
+    $('.gallery-container').css('height', image_height);
+    $('.gallery-container .back').css({
+        'padding-top' : background_padding/2,
+        'padding-bottom' : background_padding/2
     });
 
-    $(".title-bar button").on('click', function(event) {
-        event.preventDefault();
-
-        // change the width of the nav.mobile to 0
-        $("nav.mobile").animate({
-            'width': '0',
-            'direction': 'left'
-        }, 500);
-    });
-
-    // mobile search bar scripts
-
-    $("button.search-button").on('click', function(event) {
-        event.preventDefault();
-
-        // toggle the visibility of search bar
-        $("#search-mobile").animate({
-            'left': '0'
-        }, 500);
-        
-        $("#search-mobile input").focus();
-
-    });
-    
-    $(".search-close").on('click', function(){
-       
-       // hide the search bar
-       $("#search-mobile").animate({
-           'left': device_width
-       }, 500);
-        
-    });
-
-    $(window).scroll(function(event) {
-        var scroll = $(window).scrollTop();
-        var mobilestate = $("#mobile-header").css('display');
-
-        if (scroll > 80) {
-            if (mobilestate != 'block') {
-                $("#sticky-header").fadeIn(100);
-                $("#sticky-search input").focus();
-            }
-            $(".scrolltop").fadeIn();
-        } else {
-            $(".scrolltop").fadeOut(100);
-            $("#sticky-header").fadeOut();
-            $("#search-desktop input").focus();
-        }
-    });
-
-    $(".scrolltop").on('click', function(event) {
-        event.preventDefault();
-
-        // scroll window to top
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
 
 });
